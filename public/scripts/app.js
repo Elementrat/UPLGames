df6cadae1f6edc2d8bd9f3fcfe1bedc1e45acbb7
Alex Sohail <elementrat@gmail.com>
2015-04-27 22:58:23 -0500

    fixed chat link port

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,17 @@
+// delegate event for performance, and save attaching a million events to each anchor
+document.addEventListener('click', function(event) {
+  var target = event.target;
+  if (target.tagName.toLowerCase() == 'a')
+  {
+      var port = target.getAttribute('href').match(/^:(\d+)(.*)/);
+      if (port)
+      {
+         target.href = port[2];
+         target.port = port[1];
+      }
+  }
+}, false);
+
 var app = angular.module("app", ["socket", "ngenter"]);
 
 
